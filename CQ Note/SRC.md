da453f579368b6ebb9f3ea29fec9afa533ee1d5b
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-13 14:17:08 +0700

    Update Config and Comps

---
+++ CQ Note/SRC.md
@@ -0,0 +1,12 @@
+# SRC 
+
+## YT
+https://www.youtube.com/watch?v=lEflo_sc82g&t=591s
+
+Time:
+- Installation: 0-18.30 
+- Start Project: 18.30 + 
+
+
+## Git Repo
+https://github.com/adrianhajdin/healthcare
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+import Image from 'next/image';
+import PatientForm from './Components/PatientForm';
+import Link from 'next/link';
+
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl ">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen black-theme">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
